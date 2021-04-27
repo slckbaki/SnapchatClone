9424de0b09a1b8f8909d52d6420cc28554cae32c
selcuk baki <selcukbaki@windowslive.com>
2021-04-28 00:56:36 +0300

    Initial Commit

---
+++ PR31-SnapchatClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PR31-SnapchatClone
+//
+//  Created by Selcuk Baki on 28/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
